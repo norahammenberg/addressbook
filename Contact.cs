c6031870e58259bf52357645ee1896d5efcd8ca0
norahammenberg <nora_kerstin@hotmail.com>
2024-04-17 16:18:34 +0100

    added class contact

---
+++ Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace addressbook
+
+{
+    public class Contact()
+    {
+        // getters and setters / properties
+        public int ContactId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        //navigation prim/ foreng key.
+    }
+}
